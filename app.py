31c1c289784d6cca5f3269095a932d8bc017b9ad
Ramish93 <ramishshah93@gmail.com>
2021-08-05 22:27:10 +0000

    added login functionality, and validation from DB

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ def index():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
-        #checks if name exists in database
+        # checks if name exists in database
         name_exists = mongo.db.user_login.find_one(
             {'username': request.form.get('username').lower()})
-    
+ 
         if name_exists:
             flash('User-Name already exists, please choose another name')
             return redirect (url_for('register'))
@@ -41,10 +41,37 @@ def register():
             'password': generate_password_hash(request.form.get('password'))
         }
         mongo.db.user_logins.insert_one(register)
-        #puts new user into session
+        # puts new user into session
         session['user'] = request.form.get('username').lower() 
         flash('Registration was successful')
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # checks if name exists
+        name_exists = mongo.db.user_logins.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if name_exists:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                name_exists["password"], request.form.get('password')):
+                    session["user"] = request.form.get('username').lower()
+                    flash('Welcome Again!')
+            else:
+                # if invalid password 
+                flash('Incorrect Username and/or Password')
+                return redirect(url_for("login"))
+
+        else:
+            # if name doesn't match
+            flash('Incorrect Username and/or Password')
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
