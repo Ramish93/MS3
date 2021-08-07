f1ce9375457dce28a48b3f18d5ef0817c7f8dfe5
Ramish93 <ramishshah93@gmail.com>
2021-08-07 19:20:03 +0000

    added datepicker to form on landing page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
     $(".sidenav").sidenav();
     $('input#input_text, textarea#textarea2').characterCounter();
+    $('.datepicker').datepicker({
+        format: 'dd mm, yyyy',
+        yearRange: 45,
+        autoClose: true,
+        showClearBtn: true,
+        i18n: {
+            done: 'Select'
+        }
+    });
 });
