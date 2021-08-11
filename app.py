c5539b75bc7db8864bcf314a4ef53a90f7435fc0
Ramish93 <ramishshah93@gmail.com>
2021-08-11 00:43:33 +0000

    trying to solve heroku deployment issue

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, Response, make_response
+    Flask, flash, render_template, Response, make_response,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
